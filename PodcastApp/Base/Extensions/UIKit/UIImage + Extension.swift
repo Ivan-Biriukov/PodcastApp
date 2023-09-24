634947bbb94185b0b12caf8620e670298f0a02a4
koko6a <rus.baranovskiy@gmail.com>
2023-09-24 20:12:25 +0300

    add images

--- PodcastApp/Base/Extensions/UIKit/UIImage + Extension.swift
+++ PodcastApp/Base/Extensions/UIKit/UIImage + Extension.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 extension UIImage {
+    
+    // MARK: - TabBarImage
+    
     struct TabBarImage {
         static var boockMarkIcon: UIImage? {
             return UIImage(named: "bookmarkIcon")
@@ -24,6 +27,148 @@ extension UIImage {
         
         static var selectedSettingsIcon: UIImage? {
             return UIImage(named: "selectedSettingsIcon")
+        }
+    }
+    
+    // MARK: - AuthImage
+    
+    struct Auth {
+        static var eyeImage: UIImage? {
+            return UIImage(named: "eyeImage")
+        }
+        
+        static var eyeClosedImage: UIImage? {
+            return UIImage(named: "eyeClosedImage")
+        }
+        
+        static var googleImage: UIImage? {
+            return UIImage(named: "googleImage")
+        }
+    }
+    
+    struct Onboarding {
+        static var firstImage: UIImage? {
+            return UIImage(named: "firstImage")
+        }
+        
+        static var secondImage: UIImage? {
+            return UIImage(named: "secondImage")
+        }
+        
+        static var thirdImage: UIImage? {
+            return UIImage(named: "thirdImage")
+        }
+    }
+    
+    // MARK: - HomeImage
+    
+    struct Home {
+        static var likeImage: UIImage? {
+            return UIImage(named: "likeImage")
+        }
+        
+        static var activeLikeImage: UIImage? {
+            return UIImage(named: "activeLikeImage")
+        }
+        
+        static var fireImage: UIImage? {
+            return UIImage(named: "fireImage")
+        }
+        
+        static var playImage: UIImage? {
+            return UIImage(named: "playImage")
+        }
+        
+        static var nextImage: UIImage? {
+            return UIImage(named: "nextImage")
+        }
+        
+        static var previousImage: UIImage? {
+            return UIImage(named: "previousImage")
+        }
+        
+        static var repeatImage: UIImage? {
+            return UIImage(named: "repeatImage")
+        }
+        
+        static var shuffleImage: UIImage? {
+            return UIImage(named: "shuffleImage")
+        }
+    }
+    
+    // MARK: - MainImage
+    
+    struct Main {
+        static var backImage: UIImage? {
+            return UIImage(named: "backImage")
+        }
+        
+        static var closeSquareImage: UIImage? {
+            return UIImage(named: "closeSquareImage")
+        }
+        
+        static var emptyImage: UIImage? {
+            return UIImage(named: "emptyImage")
+        }
+        
+        static var plusIcon: UIImage? {
+            return UIImage(named: "plusIcon")
+        }
+        
+        static var searchImage: UIImage? {
+            return UIImage(named: "searchImage")
+        }
+        
+        static var selectedPlusIcon: UIImage? {
+            return UIImage(named: "selectedPlusIcon")
+        }
+        
+        static var threeDotsImage: UIImage? {
+            return UIImage(named: "threeDotsImage")
+        }
+    }
+    
+    // MARK: - SettingsImage
+    
+    struct Settings {
+        static var albumIcon: UIImage? {
+            return UIImage(named: "albumIcon")
+        }
+        
+        static var basketIcon: UIImage? {
+            return UIImage(named: "basketIcon")
+        }
+        
+        static var calendarIcon: UIImage? {
+            return UIImage(named: "calendarIcon")
+        }
+        
+        static var editPhotoImage: UIImage? {
+            return UIImage(named: "editPhotoImage")
+        }
+        
+        static var fileIcon: UIImage? {
+            return UIImage(named: "fileIcon")
+        }
+        
+        static var lockImage: UIImage? {
+            return UIImage(named: "lockImage")
+        }
+        
+        static var profileImage: UIImage? {
+            return UIImage(named: "profileImage")
+        }
+        
+        static var selectedCheckmark: UIImage? {
+            return UIImage(named: "selectedCheckmark")
+        }
+        
+        static var shieldImage: UIImage? {
+            return UIImage(named: "shieldImage")
+        }
+        
+        static var unselectedCheckmark: UIImage? {
+            return UIImage(named: "unselectedCheckmark")
         }
     }
 }
---
+++ PodcastApp/Base/Views/BaseCollectionViewCell.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    func createStackView(
+        for views: UIView..., axis: NSLayoutConstraint.Axis,
+        spacing: CGFloat,
+        distribution: UIStackView.Distribution = .fill,
+        alignment: UIStackView.Alignment = .fill
+    ) -> UIStackView {
+        let stack = UIStackView(arrangedSubviews: views)
+        stack.axis = axis
+        stack.spacing = spacing
+        stack.distribution = distribution
+        stack.alignment = alignment
+        return stack
+    }
+    
+    func createLabel(
+        text: String, font: UIFont, textColor: UIColor,
+        alignment: NSTextAlignment = .left
+    ) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.font = font
+        label.textColor = textColor
+        label.textAlignment = alignment
+        return label
+    }
+    
+    func createTitleButton(title: String, titleColor: UIColor, font: UIFont) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(titleColor, for: .normal)
+        button.titleLabel?.font = font
+        return button
+    }
+}
