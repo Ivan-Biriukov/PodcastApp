76dbad896d1d3974f833b545342c663fb87f9268
Ivan Biriukov <Grottt0@mail.ru>
2023-10-05 00:05:58 +0300

    Rebase HomeViewInput

--- PodcastApp/Modules/Home/View/HomeViewInput.swift
+++ PodcastApp/Modules/Home/View/HomeViewInput.swift
@@ -1,6 +1,10 @@
 import Foundation
 
 protocol HomeViewInput: AnyObject {
+    func preloadTrending(viewModel: [AllCategoryesViewModel])
+    func preloadHomeViewTableViewResults(viewModels: [HomeViewCategoryTableViewModel])
+    
+    
     func updateMainCategoryCollection(viewModels: [CategoryViewModel])
     func updateAllCategoryes(viewModels: [AllCategoryesViewModel])
     func updateTableView(viewModels: [HomeViewCategoryTableViewModel])
