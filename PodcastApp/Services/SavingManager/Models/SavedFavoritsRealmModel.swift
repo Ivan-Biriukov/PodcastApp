c890fd02bc9db839053032ae24a3b11ab742cff0
Ivan Biriukov <Grottt0@mail.ru>
2023-10-06 20:16:13 +0300

    Seems like "перелопатил" saving Manager

---
+++ PodcastApp/Services/SavingManager/Models/SavedFavoritsRealmModel.swift
@@ -0,0 +1,8 @@
+import Foundation
+import RealmSwift
+
+
+@objcMembers
+final class SavedFavoritsRealmModel: Object {
+    dynamic var id = 0
+}
---
+++ PodcastApp/Services/SavingManager/Models/SelfRealmSavedLists.swift
@@ -0,0 +1,17 @@
+import Foundation
+import RealmSwift
+
+@objcMembers
+final class SelfRealmSavedLists: Object {
+    dynamic var playlistImagePath : String = ""
+    dynamic var playlistName : String = ""
+    dynamic var episodes : [SelfRealmEpisodes] = []
+    dynamic var playlistCount : Int = 0
+}
+
+class SelfRealmEpisodes: Object {
+    dynamic var imageURLString : String = ""
+    dynamic var episodeName : String = ""
+    dynamic var authorName : String = ""
+    dynamic var duration : String = ""
+}
