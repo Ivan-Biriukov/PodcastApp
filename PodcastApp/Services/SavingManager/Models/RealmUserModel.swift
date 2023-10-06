c9f3ed2390b4ce2f749251d0118515643b8d71fa
koko6a <rus.baranovskiy@gmail.com>
2023-10-06 17:22:03 +0300

    add realm

---
+++ PodcastApp/Services/SavingManager/Models/RealmUserModel.swift
@@ -0,0 +1,18 @@
+import Foundation
+import RealmSwift
+
+final class RealmUserModel: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var username: String = ""
+    @Persisted var email: String = ""
+    @Persisted var userID: String = ""
+    @Persisted var avatarImageData: Data = Data()
+    
+    convenience init(username: String, email: String, userID: String, avatarImageData: Data = Data()) {
+        self.init()
+        self.username = username
+        self.email = email
+        self.userID = userID
+        self.avatarImageData = avatarImageData
+    }
+}
